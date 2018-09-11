b8c28f74b0e96534f0b53bcfdd227399cb9faac6
derickcastillo38 <9derick@gmail.com>
2018-09-11 09:54:28 -0400

    updated cuisine controller and route

--- app/controllers/api/v1/cuisines_controller.rb
+++ app/controllers/api/v1/cuisines_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::CuisinesController < ApplicationController
+  before_action :find_cuisine, only: [:update]
+  def index
+    @cuisines = Cuisine.all
+    render json: @cuisines
+  end
+
+  def update
+    @cuisine.update(cuisine_params)
+    if @cuisine.save
+      render json: @cuisine, status: :accepted
+    else
+      render json: { errors: @cuisine.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def cuisine_params
+    params.permit(:name)
+  end
+
+  def find_cuisine
+    @cuisine = Cuisine.find(params[:id])
+  end
 end
