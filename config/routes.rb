06d3290876477f4f203901563dda8b301e1ada9d
derickcastillo38 <9derick@gmail.com>
2018-09-12 11:31:12 -0400

    aliased routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :cuisines, only: [:index, :update]
-      resources :mealtimes, only: [:index, :update]
-      resources :recipe_cards, only: [:index, :show, :update]
+      resources :cuisines, only: [:index, :update, :create]
+      resources :meal_times, :path => "meals", only: [:index, :update]
+      resources :recipe_cards, :path => "recipes", only: [:index, :show, :update]
     end
   end
 end
